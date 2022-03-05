b9a47d00d93df8d830b31e98ea805760e5add42f
Iván Bonilla <ivanbonilla25@gmail.com>
2022-03-05 00:29:52 -0300

    Inicio de la app, creando controlador

---
+++ src/main/java/com/gestion/empleados/modelo/Empleado.java
@@ -0,0 +1,31 @@
+package com.gestion.empleados.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "empleados")
+public class Empleado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombre", length = 60, nullable = false)
+	private String nombre;
+	
+	@Column(name = "apellido", length = 60, nullable = false)
+	private String apellido;
+	
+	@Column(name = "email", length = 60, nullable = false, unique = true)
+	private String email;
+	
+	public Empleado() {
+		
+	}
+
+}
